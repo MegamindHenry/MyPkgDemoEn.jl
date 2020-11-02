6aa2d88535c5c4b61471f21209774306cc8505ae
Xuefeng <hakunamatatahenry@gmail.com>
2020-11-02 14:54:13 -0500

    add first function

--- src/MyPkgDemoEn.jl
+++ src/MyPkgDemoEn.jl
@@ -1,5 +1,26 @@
 module MyPkgDemoEn
 
-# Write your package code here.
+"""
+Add numbers together.
+"""
+function add_nums end
+
+"""
+    add_nums(a,b)
+
+Add two numbers.
+"""
+function add_nums(a,b)
+  a+b
+end
+
+"""
+    add_nums(a,b,c)
+
+Add three numbers.
+"""
+function add_nums(a,b,c)
+  a+b+c
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "MyPkgDemoEn.jl" begin
     # Write your tests here.
+    @test MyPkgDemoEn.add_nums(1,3) == 4
+    @test MyPkgDemoEn.add_nums(2,3) == 5
+    @test MyPkgDemoEn.add_nums(3,4) == 7
+    @test MyPkgDemoEn.add_nums(3,4,1) == 8
+    @test MyPkgDemoEn.add_nums(3,4,2) == 9
+    @test MyPkgDemoEn.add_nums(2,4,1) == 7
 end
